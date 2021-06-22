a3c598996d890fc67462ca98add686095a033ccb
kduczek <duczek.krystian@gmail.com>
2021-06-22 22:02:23 +0200

    Created main page method to fetch hardest recipes from DB

--- app/src/main/java/com/example/delicioso/MainActivity.java
+++ app/src/main/java/com/example/delicioso/MainActivity.java
@@ -19,6 +19,7 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.chip.Chip;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 import com.google.firebase.storage.FirebaseStorage;
@@ -41,12 +42,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.home_page);
 
         getEasiestRecipes();
+        getHardestRecipes();
     }
 
     private void getEasiestRecipes() {
         db.collection("Recipes")
                 .orderBy("Difficulty")
-                .limit(10)
+                .limit(5)
                 .get()
                 .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
                     @Override
@@ -57,8 +59,31 @@ public class MainActivity extends AppCompatActivity {
                                 easiestRecipes.add(new HomeRecipeListItem(document.getString("Name"), path));
                             }
                             LinearLayout linearLayout;
-                            linearLayout = findViewById(R.id.RecipesLinearLayout);
+                            linearLayout = findViewById(R.id.easiestRecipesLinearLayout);
                             addToView(easiestRecipes, linearLayout);
+                        } else {
+                            Log.d(TAG, "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
+    }
+
+    private void getHardestRecipes() {
+        db.collection("Recipes")
+                .orderBy("Difficulty", Query.Direction.DESCENDING)
+                .limit(5)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull @org.jetbrains.annotations.NotNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : Objects.requireNonNull(task.getResult())) {
+                                String path = document.getString("Photo_link");
+                                hardestRecipes.add(new HomeRecipeListItem(document.getString("Name"), path));
+                            }
+                            LinearLayout linearLayout;
+                            linearLayout = findViewById(R.id.hardestRecipesLinearLayout);
+                            addToView(hardestRecipes, linearLayout);
                         } else {
                             Log.d(TAG, "Error getting documents: ", task.getException());
                         }
